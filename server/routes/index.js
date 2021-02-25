05e870eb669fc93104d1ec4200d138492033919f
Michael Johansson <mikejoh12@gmail.com>
2021-02-24 21:21:58 -0600

    document customer user routes with swagger

--- server/routes/index.js
+++ server/routes/index.js
@@ -66,15 +66,124 @@ router
  */
     .post('/auth/login', validateLogin, auth.loginUser) //Logs user in and sends a JWT back in cookie
     
+/**
+ * @openapi
+ * definitions:
+ *   Product:
+ *     properties:
+ *       id:
+ *         type: integer
+ *       name:
+ *         type: string
+ *       price:
+ *         type: decimal
+ *       description:
+ *         type: string
+ *       category:
+ *         type: string
+ *       image_url:
+ *         type: string
+ *       status:
+ *         type: string
+ */
+
+/**
+ * @openapi
+ * /api/products:
+ *   get:
+ *     description: Returns all products
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of all products
+ *         schema:
+ *           $ref: '#/definitions/Product'
+ */
     .get('/products', products.getAllProducts)
+
+/**
+ * @openapi
+ * /api/products/{id}:
+ *   get:
+ *     description: Returns one products
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         in: params
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: One product object
+ *         schema:
+ *           $ref: '#/definitions/Product'
+ */
     .get('/products/:id', validateGetProducts, products.getProductById)
+
     .post('/products', validatePostProduct, passport.authenticate('jwt-admin', {session: false}), products.postProduct)
     .put('/products/:id', validatePutProduct, passport.authenticate('jwt-admin', {session: false}), products.putProduct)
     .delete('/products/:id', validateDeleteProduct, passport.authenticate('jwt-admin', {session: false}), products.deleteProduct)
 
     .get('/users', passport.authenticate('jwt-admin', {session: false}), users.getAllUsers)
+    
+/**
+ * @openapi
+ * /api/users/self:
+ *   get:
+ *     description: Returns info about the logged in user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         in: JWT cookie
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Returns object containing user info
+ */
     .get('/users/self', passport.authenticate('jwt-customer', {session: false}), users.getUserSelf) //Customer can access their user info
+
+/**
+ * @swagger
+ * /api/users/self:
+ *   put:
+ *     description: Updates the logged in user
+ *     produces: application/json
+ *     parameters:
+ *             - name: id
+ *               in: JWT cookie
+ *               required: true
+ *             - name: email
+ *               in: body
+ *               required: true
+ *             - name: first_name
+ *               in: body
+ *               required: true    
+ *             - name: last_name
+ *               in: body
+ *               required: true
+ *             - name: address1
+ *               in: body
+ *               required: true
+ *             - name: address2
+ *               in: body
+ *               required: true
+ *             - name: postcode
+ *               in: body
+ *               required: true   
+ *             - name: city
+ *               in: body
+ *               required: true
+ *             - name: country
+ *               in: body
+ *               required: false
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
     .put('/users/self', validatePutUser, passport.authenticate('jwt-customer', {session: false}), users.putUserSelf) //Customer can edit their user info
+    
     .delete('/users/:id', validateDeleteUser, passport.authenticate('jwt-admin', {session: false}), users.deleteUser) //Todo: Delete related cart for user
 
     .get('/carts', passport.authenticate('jwt-admin', {session: false}), carts.getAllCarts) //Gets all products in all carts
