0a63e89028df1e22695e0334173d478b674b4d8c
Michael Johansson <mikejoh12@gmail.com>
2021-02-27 06:43:04 -0600

    initiate client with CRA

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          E-Market
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
