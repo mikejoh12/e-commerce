511cbaae082726de2b0b34aafafcaba27bfe2e80
Michael Johansson <mikejoh12@gmail.com>
2021-03-01 06:07:43 -0600

    adjust md grid columns for products

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -9,7 +9,7 @@ const MainContent = () => {
   
   return (
       <div className="flex-grow p-5">
-            <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5 max-w-7xl mx-auto">
+            <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-5 max-w-7xl mx-auto">
               {cards} 
             </div>
       </div>
