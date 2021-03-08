2d3096772464e54256f424ecc6ef0cec5331cdfa
Michael Johansson <mikejoh12@gmail.com>
2021-03-08 06:26:33 -0600

    add notice on empty cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -19,11 +19,19 @@ const Cart = () => {
                                 )}
           <div>
             <div>
+              { (Object.keys(cartContents).length) ?
               <Link to="/checkout">
                 <button className="m-4 mt-4 py-2 px-4 cursor-pointer border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                       Go to Checkout
                 </button>   
               </Link>
+              :
+              <div>
+                  <p className="font-bold text-xl mb-2 text-gray-700 text-base">
+                    The cart is empty.
+                  </p>
+              </div>
+              }
               </div>
             </div>     
           </div>
