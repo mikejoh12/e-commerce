e6a358d069dc414dcb6f571db088573ec33a6338
Michael Johansson <mikejoh12@gmail.com>
2021-03-08 13:32:37 -0600

    change order list from <ul> to grid

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -11,7 +11,7 @@ const Account = () => {
     return (
         <div className="flex-grow p-5">
           <div className="grid justify-center">
-            <div className="m-4">
+            <div className="mx-auto">
               <h1>Account info:</h1>
               <ul>
                   <li>{`${user.first_name} ${user.last_name}`}</li>
@@ -22,17 +22,17 @@ const Account = () => {
                   <li>{user.country}</li>
               </ul>
             </div>
+            
             <div className="m-4">
-              <h1>Order History</h1>
-              <ul>
-
-                {Object.keys(orders).map(keyName =>
+              <h1 className="text-center">Order History</h1>
+              <div className="p-10 grid grid-cols-1 gap-5 max-w-7xl mx-auto">
+              {Object.keys(orders).map(keyName => 
                     orders[keyName].map(orderItem => {
-                      return <li key={`${keyName}-${orderItem.product_id}`}
-                        >{`${orderItem.order_id} - ${orderItem.name} - ${orderItem.price} - ${orderItem.created_at}`}</li>
-                    })
-                  )}
-              </ul>
+                     return <div key={`${orderItem.order_id}-${orderItem.product_id}`}
+                  >{`${orderItem.order_id} - ${orderItem.name} - ${orderItem.price} - ${orderItem.created_at}`}</div>
+                  })
+                )}
+              </div>
             </div>
           </div>
         </div>
