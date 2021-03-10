bfd21238fbeea93a5b7952bab2bb7c852016c66c
Michael Johansson <mikejoh12@gmail.com>
2021-03-09 22:42:05 -0600

    add cart counter but still need to fix bug in it

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,17 +1,19 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 import { selectIsLoggedIn } from '../features/users/usersSlice'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { isLoggedInUpdated, currentUserUpdated } from '../features/users/usersSlice'
-import { cartProductsUpdated } from '../features/cart/cartSlice'
+import { cartProductsUpdated, selectCart } from '../features/cart/cartSlice'
+import { useEffect } from 'react'
 const axios = require('axios')
 
 function Nav() {
 
     const isLoggedIn = useSelector(selectIsLoggedIn)
     const dispatch = useDispatch()
+    const cart = useSelector(selectCart)
+
+    const nrCartItems = Object.keys(cart).reduce((acc, keyName) => 
+    acc + cart[keyName].quantity, 0)
 
     const handleLogout = async() => {
       try {
@@ -31,12 +33,20 @@ function Nav() {
                 <p className="text-4xl text-white text-center font-mono whitespace-nowrap mx-2">E-Market</p>
               </Link>
               <p className="hidden sm:inline text-md text-white text-center font-mono mx-2">A demo e-commerce app</p>
+              
               <Link to="/cart">
-                <span className="inline">
-                  <FontAwesomeIcon icon={faShoppingCart} size="lg" inverse/>
-                  <p className="inline text-md text-white text-center font-mono mx-2">Cart</p>
-                </span>
+                <button className="py-4 px-1 relative border-2 border-transparent text-white rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
+                  <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                  </svg>
+                  <span className="absolute inset-0 object-right-top -mr-6">
+                    <div className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
+                      {nrCartItems}
+                    </div>
+                  </span>
+                </button>
               </Link>
+
               {isLoggedIn ?
               <div>
                 <Link to="/account">
