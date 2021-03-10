bba8015a3ef0f3f528e0d332aacd1cfe3d563f1f
Michael Johansson <mikejoh12@gmail.com>
2021-03-09 21:33:55 -0600

    make add to cart button increment qty up to 10

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { addProductToCart } from '../features/cart/cartSlice'
+import { addProductToCart, changeProductQuantity, selectCart } from '../features/cart/cartSlice'
 import { useHistory } from 'react-router-dom' 
 import { selectIsLoggedIn } from '../features/users/usersSlice'
 
@@ -9,19 +9,30 @@ const ProductCard = ({product}) => {
     const dispatch = useDispatch()
     const history = useHistory()
     const isLoggedIn = useSelector(selectIsLoggedIn)
+    const cartContents = useSelector(selectCart)   
 
-    const handleAddToCartClicked = async () => {
+    //Return true if product is already in cart
+    const isProductInCart = () => cartContents.hasOwnProperty(product.id)
+
+    const handleAddToCartClick = async () => {
         if (!isLoggedIn) {
             history.push('/login')
             return
         }
         try {
-            await dispatch(
+            if (isProductInCart()) {
+                console.log('Already in cart')
+                dispatch(changeProductQuantity({
+                    product_id: product.id,
+                    quantity: (cartContents[product.id].quantity + 1 >= 10) ? 10 : cartContents[product.id].quantity + 1
+                }))
+            } else {
+            dispatch(
                 addProductToCart({
                     product_id: product.id,
                     quantity: 1
                 })
-            )
+            )}
         } catch (err) {
             console.error('Failed to add to cart: ', err)
         }
@@ -42,8 +53,8 @@ const ProductCard = ({product}) => {
                 </Link>
                     <div className="px-6 pt-4 pb-2">
                         <div className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">${product.price}</div>
-                        <button   className="inline-block z-10 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                                onClick={handleAddToCartClicked}>Add to cart</button>
+                        <button   className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                                onClick={handleAddToCartClick}>Add to cart</button>
                     </div>
                 </div>
             </div>
