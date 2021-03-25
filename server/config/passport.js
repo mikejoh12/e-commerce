7e979039158a20bde5c17101defe82d95fb502f7
Michael Johansson <mikejoh12@gmail.com>
2021-03-24 21:26:40 -0500

    removed facebook login code

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,7 +1,6 @@
 const passport = require('passport')
 const bcrypt = require('bcrypt')
 const LocalStrategy = require('passport-local').Strategy
-const FacebookStrategy = require('passport-facebook').Strategy
 const usersService = require('../services/users.service')
 const JWTstrategy = require('passport-jwt').Strategy;
 const ExtractJWT = require('passport-jwt').ExtractJwt;
@@ -33,49 +32,6 @@ passport.use(
     }
 }
 ))
-
-passport.use(new FacebookStrategy({
-  clientID: process.env.FACEBOOK_APP_ID,
-  clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "http://localhost:3000/api/auth/facebook/callback",
-  profileFields: ['id', 'email']
-},
-async (accessToken, refreshToken, profile, done) => {
-  /*
-  User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-    return cb(err, user);
-  })*/
-  console.log('Accessing facebook passport config function')
-  return done(null, profile)
-  /*
-  try {
-    const user = await usersService.fetchUserByFacebookId(profile.id)
-    if (!user) {
-      //Create user
-      const user = {
-        email: profile.email,
-        facebook_id: profile.id,
-        first_name: 'test',
-        last_name: 'test',
-        address1: 'test',
-        address2,
-        postcode: 'test',
-        city: 'test',
-        country,
-        pwd_hash: 'test',
-        user_role: "customer"
-      }        
-      const newUser = await createUser(user)
-      const newCart = await createCart(newUser.id)
-      return done(null, profile)
-    } else if (user) {
-      return done(null, profile)
-    }
-  } catch (error) {
-    return done(error)
-  }
-  */
-}))
 
 //Checks the A_JWT cookie
 passport.use(
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,8 +9,6 @@ const router = express.Router()
 router
     .post('/auth/signup', validateSignUp, auth.signupUser) //Adds a user and creates a cart for the user
     .post('/auth/login', validateLogin, auth.loginUser) //Logs user in and sends a JWT back in cookie
-    .get('/auth/facebook', passport.authenticate('facebook'))
-    .get('/auth/facebook/callback', auth.facebookCallback)
     .post('/auth/logout', auth.logoutUser) //Deletes httpOnly cookie to logout
 
     .get('/products', products.getAllProducts)
