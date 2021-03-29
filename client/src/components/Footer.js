f7563e6d43793bc2480f8af7519e203d5f22fb3b
Michael Johansson <mikejoh12@gmail.com>
2021-03-28 22:20:14 -0500

    nav and footer ui improvements

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCopyright } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
     return (
       <div className="bg-blue-100 p-5">
-          <div className="text-center">
+          <div className="m-2 text-center">
             <span className="p-2">
               <FontAwesomeIcon icon={faCopyright} size="lg"/>
             </span>
             <p className="inline text-xl font-mono">2021 E-Market</p>
+          </div>
+          <div className="m-2 text-center">
+            <a href="https://github.com/mikejoh12/e-commerce">
+              <span className="p-2">
+                  <FontAwesomeIcon icon={faGithub} size="lg"/>
+              </span>
+              <p className="inline text-xl font-mono">Git Repo</p>
+            </a>
           </div>
       </div>
     )
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -3,6 +3,8 @@ import { selectIsLoggedIn } from '../features/users/usersSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { isLoggedInUpdated, currentUserUpdated } from '../features/users/usersSlice'
 import { cartProductsUpdated, selectCart } from '../features/cart/cartSlice'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 const axios = require('axios')
 
 function Nav() {
@@ -28,39 +30,51 @@ function Nav() {
     return (
       <div className="bg-black">
         <div className="max-w-7xl mx-auto flex flex-row justify-around items-center p-5">
-              <Link to="/">
-                <p className="text-4xl text-white text-center font-mono whitespace-nowrap mx-2">E-Market</p>
-              </Link>
-              <p className="hidden sm:inline text-md text-white text-center font-mono mx-2">A demo e-commerce app</p>
-              
-              <Link to="/cart">
-                <button className="py-4 px-1 relative border-2 border-transparent text-white rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
-                  <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                  </svg>
-                  <span className="absolute inset-0 object-right-top -mr-6">
-                    <div className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
-                      {nrCartItems}
-                    </div>
-                  </span>
-                </button>
-              </Link>
-
-              {isLoggedIn ?
-              <div>
-                <Link to="/account">
-                  <button   className="text-md text-white text-center font-mono mx-4"
-                            >User Account</button>
-                </Link>
+              <div className="m-2">
                 <Link to="/">
-                <button   className="text-md text-white text-center font-mono mx-2"
-                          onClick={handleLogout}>Sign Out</button>
+                  <p className="text-4xl text-white text-center font-mono whitespace-nowrap mx-2">E-Market</p>
                 </Link>
               </div>
+
+              <p className="hidden sm:inline text-md text-white text-center font-mono mx-2">A demo e-commerce app</p>
+              
+              <div className="m-2">
+                <Link to="/cart">
+                  <button className="py-4 px-1 relative border-2 border-transparent text-white rounded-full hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Cart">
+                    <svg className="h-6 w-6" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                      <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                    </svg>
+                    <span className="absolute inset-0 object-right-top -mr-6">
+                      <div className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
+                        {nrCartItems}
+                      </div>
+                    </span>
+                  </button>
+                </Link>
+              </div>
+
+              {isLoggedIn &&
+              <div className="m-2">
+                <Link to="/account">
+                  <FontAwesomeIcon icon={faUserCircle} size="lg" inverse/>
+                  <button   className="hidden md:inline text-md text-white text-center font-mono ml-2"
+                            >User Account</button>
+                </Link>
+              </div>
+              }
+              {isLoggedIn ?
+                <div className="m-2">
+                  <Link to="/">
+                  <button   className="text-md text-white text-center font-mono mx-2"
+                            onClick={handleLogout}>Sign Out</button>
+                  </Link>
+                </div>
               :
+              <div className="m-2">
               <Link to="/login">
                 <button className="text-md text-white text-center font-mono mx-4">Sign In / Register</button>
               </Link>
+              </div>
               }           
         </div>
       </div>
